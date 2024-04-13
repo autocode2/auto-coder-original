2ee7458cf6ee43129c335a6dd9e7cea7b8990968
Gareth Andrew <gingerhendrix@gmail.com>
2024-04-14 00:15:23 +0100

    Autocoder: Add CDATA to prompt instructions

--- src/sendAIMessage.ts
+++ src/sendAIMessage.ts
@@ -15,18 +15,18 @@ const rl = readline.createInterface({
 
 async function main() {
   const systemPrompt = `You are an AI coding tool. Help the user with their coding tasks using the output format given.
-You will be given information about the current project in a &lt;Context&gt;&lt;/Context&gt; element.  This will include the full contents of every file in the project, using &lt;File&gt;&lt;/File&gt; elements.
+You will be given information about the current project in a <Context></Context> element.  This will include the full contents of every file in the project, using <File></File> elements.
 Output your response using the following XML.
-&lt;Output&gt;&lt;/Output&gt; - This is the root tag, your response must contain exactly one of these elements.
-The &lt;Output&gt;&lt;/Output&gt; element contains a list of the following elements:
-&lt;Thinking&gt;&lt;/Thinking&gt; - You may find it useful to use this space to think about the the user's request and to make a plan before making an action.
-&lt;Message&gt;&lt;/Message&gt; - Use this element to display a message to the user. Write the message inside the tags, you may use markdown for formatting.
-&lt;Command&gt;&lt;/Command&gt; - Execute the given command.  You may assume a unix operating system and the current working directory is set to the project root.
-&lt;Patch&gt;&lt;/Patch&gt; - Include any code to be altered.  Use the following attributes to add metadata about the patch:
+<Output></Output> - This is the root tag, your response must contain exactly one of these elements.
+The <Output></Output> element contains a list of the following elements:
+<Thinking></Thinking> - You may find it useful to use this space to think about the the user's request and to make a plan before making an action.
+<Message></Message> - Use this element to display a message to the user. Write the message inside the tags, you may use markdown for formatting.
+<Command></Command> - Execute the given command.  You may assume a unix operating system and the current working directory is set to the project root.
+<Patch></Patch> - Include any code to be altered.  Use the following attributes to add metadata about the patch:
  - filename - the name of the file to be changed
  
  - type - use "change" for an existing file,  "create" for a new file, and  "delete" without any contents to delete a file.
-Use HTML entity encoding where required to output valid html e.g. encode \`&lt;\` as \`&amp;lt;\` and \`&gt;\` as \`&amp;gt;\`
+Wrap the contents of <Message>, <Command>, and <Patch> tags in CDATA sections.
 `;
 
   const filePaths = await getGitFiles();
