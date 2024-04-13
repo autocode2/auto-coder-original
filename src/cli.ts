96ce2557646e51945d59ae5f5ba0efad089267f7
Gareth Andrew <gingerhendrix@gmail.com>
2024-04-14 00:42:54 +0100

    Autocoder: Add commander.js

--- src/cli.ts
+++ src/cli.ts
@@ -1,34 +1,29 @@
+import { program } from 'commander';
 import { generateXmlInput, getGitFiles } from './generateInput';
 import { parseXmlOutput } from './parseOutput';
 import { sendMessage } from './sendAIMessage';
 
-async function main() {
-  const command = process.argv[2];
+program
+  .command('generate-input')
+  .description('Generate XML input from Git files')
+  .action(async () => {
+    const filePaths = await getGitFiles();
+    const xml = await generateXmlInput(filePaths);
+    console.log(xml);
+  });
 
-  switch (command) {
-    case 'generate-input':
-      const filePaths = await getGitFiles();
-      const xml = await generateXmlInput(filePaths);
-      console.log(xml);
-      break;
-    
-    case 'send-message':
-      await sendMessage();
-      break;
-    
-    case 'parse-output':
-      const xmlFilePath = process.argv[3];
-      if (!xmlFilePath) {
-        console.error('Please provide an XML file path.');
-        return;
-      }
-      await parseXmlOutput(xmlFilePath);
-      break;
+program
+  .command('send-message')
+  .description('Send a message to the AI and parse the response')
+  .action(async () => {
+    await sendMessage();
+  });
 
-    default:
-      console.error(`Unknown command: ${command}`);
-      console.error('Available commands: generate-input, send-message, parse-output');
-  }
-}
+program
+  .command('parse-output <xmlFilePath>')
+  .description('Parse XML output file')
+  .action(async (xmlFilePath) => {
+    await parseXmlOutput(xmlFilePath);
+  });
 
-main();
+program.parse();
